4967f9c18c1a1a29b8a080fd320b47f3394acb92
Pokey Rule <pokey.rule@gmail.com>
2017-02-10 12:53:41 +0000

    Initial pubmedasync

--- main.py
+++ main.py
@@ -1,37 +1,47 @@
 import csv
 import sys
-from itertools import chain
+import asyncio
+from asyncio import as_completed
 
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+import aiohttp
 
-from fetch import Fetcher
-from process import extract_authors, fieldnames
+from pubmedasync.fetch import Fetcher
+from pubmedasync.process import extract_authors, fieldnames
 
 # Get term from command-line args
 term = " ".join(sys.argv[1:])
 
+
 # Get list of results
-fetcher = Fetcher(term)
-fetcher.search()
+async def main():
+    conn = aiohttp.TCPConnector(limit=10)
+    async with aiohttp.ClientSession(connector=conn) as session:
+        fetcher = Fetcher(session, term)
+        await fetcher.search()
 
 # Iterate through all the pages and extract all authors
-results = []
-with tqdm(total=fetcher.total, unit='paper') as pbar:
-    for data in fetcher.get_pages():
-        soup = BeautifulSoup(data, "lxml")
-        papers = soup.find_all('pubmedarticle')
+        results = []
+        with tqdm(total=fetcher.total, unit='paper') as pbar:
+            for data in as_completed(fetcher.get_pages()):
+                soup = BeautifulSoup(await data, "lxml")
+                papers = soup.find_all('pubmedarticle')
 
-        for paper in papers:
-            results += extract_authors(paper)
+                for paper in papers:
+                    results += extract_authors(paper)
 
-        # Update progress bar
-        pbar.update(len(papers))
+                # Update progress bar
+                pbar.update(len(papers))
 
 # Output information to csv file
-with open('out.csv', 'w') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        with open('out.csv', 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-    writer.writeheader()
-    for result in results:
-        writer.writerow(result)
+            writer.writeheader()
+            for result in results:
+                writer.writerow(result)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+loop.close()
